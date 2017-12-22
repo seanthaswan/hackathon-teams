286913c419acba7e15ba9ed258033d2a55ffde32
seanthaswan <owneruser@Owners-MacBook-Pro.local>
2017-12-22 08:50:47 -0800

    Hackathon Teams Node App Practice

--- controllers/teams.js
+++ controllers/teams.js
@@ -3,10 +3,32 @@ var fs = require('fs');
 var teamService = require('../models/teamService');
 var router = express.Router();
 
+// var db = require('./models');
+
 router.get('/', function(req, res) {
   var teams = teamService.allTeams();
   res.render('teams/index', { teams: teams });
 });
+
+router.delete('/:name', function(req,res){
+	console.log("Deleted route name = ", req.params.name);
+	teamService.deleteTeam(req.params.name);
+	res.send();
+	});
+
+// router.delete('/:id', function(req,res){
+// 	console.log('Delete route', req.params.id);
+// 	teams.destroy({
+// 		where: { id: req.params.id }
+// 	}).then(function(deleted){
+// 		console.log('deleted = ', deleted);
+// 		res.send('sucess');
+// 	}).catch(function(err){
+// 		console.log('An error happened', err);
+// 		res.send('fail');
+// 	})
+// });
+
 
 router.post('/', function(req, res) {
   teamService.addTeam(req.body);
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-
+$('.delete-link').click(function(e){
+	e.preventDefault();
+	console.log("hi");
+	$.ajax({
+		url: $(this).attr('href'),
+		method: 'DELETE'
+	}).success(function(data){
+		window.location.href = '/teams';
+	});
 });
+
